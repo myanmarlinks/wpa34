1cdfd4ed2e552c9b6039d005c806045aa101cf59
phpcrazy <phpcrazy@gmail.com>
2020-08-22 18:35:29 +0630

    Don't Repeat

--- basicphp/public/index.php
+++ basicphp/public/index.php
@@ -1,12 +1,19 @@
 <?php
+    // Separation of Concern
+    // K.I.S.S Keep it Simple, Stupid
+    // Semantic
+    // Don't repeat
     if(isset($_GET['name'])) {
         $name = $_GET['name'];
-        if($name == "Aung") {
-            require "../view/aung.php";
-        } else {
-            require "../view/thiha.php";
-        }
+        get_view($name);
     } else {
-        require "../view/home.php";
+        get_view("home");
+    }
+
+    function get_view(string $title) {
+        ob_start();
+        $title = $title;
+        require "../view/" . strtolower($title) . ".php";
+        ob_end_flush();
     }
 ?>
